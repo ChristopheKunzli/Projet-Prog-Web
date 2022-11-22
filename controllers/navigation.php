08aa3dcc43340591d4b499f6a4d473eecaff4d88
KUNZLI Christophe <Christophe.KUNZLI@cpnv.ch>
2022-11-22 11:38:50 +0100

    mvc

---
+++ controllers/navigation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file     navigation.php
+ * @brief    File description
+ * @author   Created by Christophe.KUNZLI
+ * @version  18.11.2022
+ */
+
+function home(): void
+{
+    require 'views/home.php';
+}
---
+++ views/home.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file     home.php
+ * @brief    File description
+ * @author   Created by Christophe.KUNZLI
+ * @version  18.11.2022
+ */
+
+echo "<h2>Hello</h2>";
